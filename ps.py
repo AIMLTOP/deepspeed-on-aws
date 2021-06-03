9c54caf48faf6470c0eb04076a7b4acd64285188
Robin Long <binlon@amazon.com>
2021-06-03 11:49:36 +0800

    add ps.py

--- ps.py
+++ ps.py
@@ -1,6 +1,6 @@
 import subprocess as sb
 
-proc_list = sb.Popen("ps -ef | awk '{print $2} ' ", stdout=sb.PIPE).communicate()[0].splitlines()
+proc_list = sb.Popen("ps -ef", stdout=sb.PIPE).communicate()[0].splitlines()
 
 for pid in proc_list:
     print(pid)
